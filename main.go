4e053e0c8b455bc5bf188948d9f8105a45ac9b11
KingKord <93544537+KingKord@users.noreply.github.com>
2023-07-10 19:35:58 +0300

    JSON

--- main.go
+++ main.go
@@ -1,21 +1,69 @@
 package main
 
 import (
-	"awesomeProject/helpers"
+	"encoding/json"
+	"fmt"
 	"log"
 )
 
-const numPool = 10
-
-func CalculateValue(intChan chan int) {
-	randomNumber := helpers.RandomNumber(numPool)
-	intChan <- randomNumber
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	HairColor string `json:"hair_color"`
+	HadDog    bool   `json:"has_dog"`
 }
-func main() {
-	intChan := make(chan int)
-	defer close(intChan)
-	go CalculateValue(intChan)
 
-	num := <-intChan
-	log.Println(num)
+func main() {
+
+	var myJson = `
+[
+    {
+        "first_name": "Wally",
+        "last_name": "West",
+        "hair_color": "red",
+        "has_dog": false
+    },
+    {
+        "first_name": "Diana",
+        "last_name": "Prince",
+        "hair_color": "black",
+        "has_dog": false
+    }
+]`
+
+	var unmarshalled []Person
+
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
+	if err != nil {
+		log.Println("Error unmarshalling json", err)
+	}
+
+	log.Printf("unmarshalled: %v", unmarshalled)
+
+	// write JSON from struct
+	var mySlice []Person
+
+	var m1 Person
+	m1.FirstName = "Wally"
+	m1.LastName = "West"
+	m1.HairColor = "red"
+	m1.HadDog = false
+
+	mySlice = append(mySlice, m1)
+
+	var m2 Person
+	m2.FirstName = "Diana"
+	m2.LastName = "Prince"
+	m2.HairColor = "black"
+	m2.HadDog = false
+
+	mySlice = append(mySlice, m2)
+
+	newJson, err := json.MarshalIndent(mySlice, "", "    ")
+	if err != nil {
+		log.Println("error marshal", err)
+	}
+
+	fmt.Println(string(newJson))
+
 }
