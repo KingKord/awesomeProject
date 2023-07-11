8a57b34899a3119be28e036117c414ae881607fd
KingKord <93544537+KingKord@users.noreply.github.com>
2023-07-11 18:13:50 +0300

    Error checking

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -16,9 +17,30 @@ func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
+}
+
+func Divide(writer http.ResponseWriter, request *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(writer, "Cannot divide by zero")
+		return
+	}
+
+	fmt.Fprintf(writer, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+
+	return result, nil
 }
 
 // About is the about page handler
