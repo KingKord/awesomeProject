dd75266b033ede803c1874c72751e21a354ab15c
KingKord <93544537+KingKord@users.noreply.github.com>
2023-07-14 17:20:13 +0300

    Some code refactoring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"awesomeProject/pkg/handlers"
 	"fmt"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
+// main is the main application function
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
