40f10a42ca76265d2bbc1a5eee89a401f2b2f891
KingKord <93544537+KingKord@users.noreply.github.com>
2023-07-26 19:51:02 +0300

    Experimenting with sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,15 +5,30 @@ import (
 	"awesomeProject/pkg/handlers"
 	"awesomeProject/pkg/render"
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main application function
 func main() {
-	var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+	// scs is the external package that helps us to work with sessions
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module awesomeProject
 
 go 1.15
 
-require github.com/go-chi/chi/v5 v5.0.10
+require (
+	github.com/alexedwards/scs/v2 v2.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
